e3f83a7377468911424258e2c8af33b6160db264
khoa9894 <khicondangyeu490@gmail.com>
2026-04-26 22:27:58 +0700

    Add sonar exclusions to fix duplication and issue count

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  preset: 'jest-expo',
+  preset: "jest-expo",
+  setupFiles: ["<rootDir>/jest.setup.js"],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
   ],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   collectCoverage: true,
-  collectCoverageFrom: [
-    'screens/**/*.{ts,tsx}',
-    '!**/node_modules/**'
-  ]
+  collectCoverageFrom: ["screens/**/*.{ts,tsx}"],
+  coverageReporters: ["lcov", "text", "clover"],
 };
