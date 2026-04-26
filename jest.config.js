37d13a99fcd2fa3f3698e8ceb6975b6f9269a2d4
ngocquandau <ngocquandau@gmail.com>
2026-04-26 20:55:26 +0700

    SonarCloud Setup w8 ver5

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   preset: 'jest-expo',
+  setupFiles: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
   ],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   collectCoverage: true,
   collectCoverageFrom: [
-    'screens/**/*.{ts,tsx}',
-    '!**/node_modules/**'
+    'screens/**/*.{ts,tsx}'
   ]
 };
--- jest.setup.js
+++ jest.setup.js
@@ -1,24 +1,11 @@
-/* eslint-disable no-undef */
+/* eslint-disable */
 
-// 1. Fix lỗi scope của Expo và sửa luôn lỗi SonarLint (S7764: Dùng globalThis)
-globalThis.__expo_module_name = 'test';
+// ĐÂY LÀ DÒNG QUAN TRỌNG NHẤT - BẮT BUỘC DÙNG 'global'
+global.__expo_module_name = 'test';
 
-// 2. Mock các thư viện Expo để không bị lỗi Font/Asset
-jest.mock('expo-font', () => ({
-  loadAsync: jest.fn(),
-  isLoaded: jest.fn(() => true),
-}));
-
-jest.mock('expo-asset', () => ({
-  Asset: {
-    loadAsync: jest.fn(),
-  },
-}));
-
-// 3. Mock Icons để UI không bị crash khi chạy test
-jest.mock('@expo/vector-icons', () => ({
-  Ionicons: 'View',
-  AntDesign: 'View',
-  MaterialIcons: 'View',
-  FontAwesome: 'View',
-}));
+// Mock Icon để giữ nguyên 100% UI HomeScreen của bạn
+jest.mock('@expo/vector-icons', () => {
+  return {
+    Ionicons: 'View',
+  };
+});
