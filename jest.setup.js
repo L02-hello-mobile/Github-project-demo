a80a4b308ec76b8544aa26cf51911323bf8ba44f
ngocquandau <ngocquandau@gmail.com>
2026-04-26 20:51:44 +0700

    SonarCloud Setup w8 ver4

--- jest.setup.js
+++ jest.setup.js
@@ -1,15 +1,24 @@
 /* eslint-disable no-undef */
-import '@testing-library/react-native/extend-expect';
 
-// Fix lỗi ReferenceError: You are trying to import a file outside of the scope
-global.__expo_module_name = 'test';
+// 1. Fix lỗi scope của Expo và sửa luôn lỗi SonarLint (S7764: Dùng globalThis)
+globalThis.__expo_module_name = 'test';
 
-// Mock toàn bộ Expo Font và Asset
-jest.mock('expo-font');
-jest.mock('expo-asset');
-jest.mock('@expo/vector-icons', () => ({
-  Ionicons: 'View',
+// 2. Mock các thư viện Expo để không bị lỗi Font/Asset
+jest.mock('expo-font', () => ({
+  loadAsync: jest.fn(),
+  isLoaded: jest.fn(() => true),
 }));
 
-// Mock phản hồi từ hệ thống để tránh treo test
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('expo-asset', () => ({
+  Asset: {
+    loadAsync: jest.fn(),
+  },
+}));
+
+// 3. Mock Icons để UI không bị crash khi chạy test
+jest.mock('@expo/vector-icons', () => ({
+  Ionicons: 'View',
+  AntDesign: 'View',
+  MaterialIcons: 'View',
+  FontAwesome: 'View',
+}));
