930fc0cdf0df7c177ad0d2fac0404f2731c63dc8
khoa9894 <khicondangyeu490@gmail.com>
2026-05-27 23:11:24 +0700

    Update testcase

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,11 @@ module.exports = {
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@sentry/.*|lucide-react-native)",
   ],
   collectCoverage: true,
-  collectCoverageFrom: ["screens/**/*.{ts,tsx}", "context/**/*.{ts,tsx}"],
+  collectCoverageFrom: [
+    "screens/**/*.{ts,tsx}",
+    "context/**/*.{ts,tsx}",
+    "services/authService.ts",
+    "services/socketService.ts",
+  ],
   coverageReporters: ["lcov", "text", "clover", "html"],
 };
